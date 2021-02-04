993307f2a53e6b0f87834b4c3002d35d08ae7525
Bernardo Gomes Negri <b.gomes.negri@gmail.com>
2021-02-04 13:26:30 -0300

    Added parallel functionality

--- CopyMapContents.py
+++ CopyMapContents.py
@@ -1,29 +1,29 @@
+import pathlib
 import zipfile
 import os
 import sys
 
 def CopyMapContents(map, output):
+    #try:
     print('Trying map ', map, ' to output: ', output)
-    try:
-        print('Trying map ', map, ' to output: ', output)
-        z = zipfile.ZipFile(map)
-        fileList = z.namelist()
+    z = zipfile.ZipFile(map)
+    fileList = z.namelist()
+    outputPath = pathlib.Path(output)
+    subdirectories = ('cfg', 'maps', 'materials', 'media', 'resource', 'scripts', 'sound', 'models')
+    for embeddedFile in fileList:
 
-        subdirectories = ('cfg', 'maps', 'materials', 'media', 'resource', 'scripts', 'sound', 'models')
-        for embeddedFile in fileList:
+        if(embeddedFile.startswith(subdirectories)):
+            #We found an asset file!
 
-            if(embeddedFile.startswith(subdirectories)):
-                #We found an asset file!
+            PathToExtract = os.path.join(outputPath)
 
-                PathToExtract = os.path.join(output)
-
-                z.extract(embeddedFile, PathToExtract)
+            z.extract(embeddedFile, PathToExtract)
         
-        print('Done map ' + map)
-    except Exception:
+    print('Done map ' + map)
+    """except Exception:
         type, value, traceback = sys.exc_info()
-        print(value)
-        print(traceback)
-        print(type)
-        print('Could not open' + str(map))
-        return map
+        print(str(value))
+        print(str(traceback))
+        print(str(type))
+        print('Could not open' + str(map))"""
+    return
--- ExtractBSPToFolderForVPK.py
+++ ExtractBSPToFolderForVPK.py
@@ -4,40 +4,47 @@ from pathlib import Path
 from CheckFolderIsValid import CheckFolderIsValid
 from CopyMapContents import CopyMapContents
 import multiprocessing
+import time
 
-print('test')
-#Use this where you want your output folder to be. Preferably empty
-OutputDirStr = input('Where should your output folder should? Preferrably empty \n')
-print('Output folder: ', OutputDirStr)
-CheckFolderIsValid(OutputDirStr)
+if __name__ == '__main__':
+    t0 = time.time()
+    #Use this where you want your output folder to be. Preferably empty
+    #OutputDirStr = input('Where should your output folder should? Preferrably empty \n')
+    OutputDirStr = "C:\\Temp\\Test\\"
+    print('Output folder: ', OutputDirStr)
+    CheckFolderIsValid(OutputDirStr)
 
-#Where the input folder is, with maps/
-InputFolderStr = input('Where your input folder is, with the subfolder /maps? \n')
-print('Input folder: ', InputFolderStr)
-CheckFolderIsValid(InputFolderStr)
+    #Where the input folder is, with maps/
+    #InputFolderStr = input('Where your input folder is, with the subfolder /maps? \n')
+    InputFolderStr = "C:\\Temp\\Test2\\"
+    print('Input folder: ', InputFolderStr)
+    CheckFolderIsValid(InputFolderStr)
 
-InputFolder = Path(InputFolderStr)
-TempDir = Path(OutputDirStr)
+    InputFolder = Path(InputFolderStr)
+    TempDir = Path(OutputDirStr)
 
-#Start by extracting all the map files and putting them into our temporary directory
-MapsFolderStr = os.path.join(InputFolderStr, 'maps\\')
-Maps = Path(MapsFolderStr).rglob('*.bsp')
-MapsList = [x for x in Maps]
+    #Start by extracting all the map files and putting them into our temporary directory
+    MapsFolderStr = os.path.join(InputFolderStr, 'maps\\')
+    Maps = Path(MapsFolderStr).rglob('*.bsp')
+    MapsList = [x for x in Maps]
 
-Arguments = []
-for m in MapsList:
-    Arguments.append((m, OutputDirStr))
+    Arguments = []
+    for m in MapsList:
+        Arguments.append((str(m), OutputDirStr))
 
 
-pool = multiprocessing.Pool()
-outcomes = pool.starmap(CopyMapContents, Arguments)
+    pool = multiprocessing.Pool()
+    pool.starmap(CopyMapContents, Arguments)
+    pool.close()
 
-#Puts the original assets in our folder
-print('Copying original files')
-for src_dir, dirs, files in os.walk(InputFolder):
-        for file_ in files:
-            src_file = os.path.join(src_dir, file_)
-            src_path = src_file.replace(InputFolderStr, '', 1)
-            dst_file = os.path.join(OutputDirStr, src_path)
-            shutil.copy(src_file, dst_file)
-print('Files ready to be packaged in VPK')
+    #Puts the original assets in our folder
+    print('Copying original files')
+    for src_dir, dirs, files in os.walk(InputFolder):
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                src_path = src_file.replace(InputFolderStr, '', 1)
+                dst_file = os.path.join(OutputDirStr, src_path)
+                shutil.copy(src_file, dst_file)
+    t1 = time.time()
+    print('Operation complete in ', round(t1 - t0, 2), ' seconds')
+    print('Files ready to be packaged in VPK')
