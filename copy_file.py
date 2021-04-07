1b872f852b33f924a212fc1e9d4f99b650aa7157
Bernardo Gomes Negri <b.gomes.negri@gmail.com>
2021-04-07 18:54:20 -0300

    Fixed issue where exception would be thrown at copying because parent directories do not exist.

--- copy_file.py
+++ copy_file.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-
+import pathlib
 
 def copy_file(file_, src_dir, input, output):
 
@@ -16,5 +16,18 @@ def copy_file(file_, src_dir, input, output):
         src_path = src_file.replace(input, '', 1).replace('\\', '', 1)
 
     dst_file = os.path.join(output, src_path)
+
+    #We need to create the subfolders in our output that do not exist
+    output_path_obj = pathlib.Path(dst_file)
+    output_parents = output_path_obj.parents
+    output_parents_trimmed = []
+    output_parents = reversed(output_parents)
+    for parent in output_parents:
+        if parent.is_relative_to(output):
+            output_parents_trimmed.append(parent)
+    for p in output_parents_trimmed:
+        if not p.exists():
+            os.mkdir(p)
+
     shutil.copy(src_file, dst_file)
     
