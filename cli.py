02934d8203428bd83bb1801d9c22f8a00cbbd832
BernardoGomesNegri <38192358+BernardoGomesNegri@users.noreply.github.com>
2021-05-24 14:37:44 -0300

    Changed style of comment

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 from core.core import main
 
 """Wrapper for CLI interface"""
-# Won't work if we run core.core.main directly
+"""Won't work if we run core.core.main directly"""
 if __name__ == '__main__':
     main()
