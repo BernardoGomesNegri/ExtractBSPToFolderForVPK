4a1791a22c6346e8d8163b70ddc7d2d6dd1f31f3
Bernardo Gomes Negri <38192358+BernardoGomesNegri@users.noreply.github.com>
2021-05-30 12:54:53 -0300

    improved handling arguments

--- core/handle_args.py
+++ core/handle_args.py
@@ -1,7 +1,14 @@
 import argparse
 import sys
+from typing import List
 from core.check_folder_is_valid import check_input_dir, check_output_dir
 from core.args import Args
+
+def interpret_arg(args: List[str], question: str) -> str:
+    for a in args:
+        if (a is not None) and (a != ''):
+            return a
+    return input(question + '\n')
 
 def parse_args(inputarg: str, outputarg: str) -> Args:
     #Prepares arguments
@@ -11,32 +18,18 @@ def parse_args(inputarg: str, outputarg: str) -> Args:
     arg_parser.add_argument('-s', '--singlethread', action="store_true", default=False)
     cmd_args = arg_parser.parse_args()
 
-    input_dir_str = ''
-    output_dir_str = ''
+    input_dir_str = interpret_arg([inputarg, cmd_args.input], 'Where is your input folder, with the subfolder /maps?')
+    output_dir_str = interpret_arg([outputarg, cmd_args.output], 'Where is your output folder, preferably empty?')
 
-    #If there is no input parameter, ask
-    if inputarg != '':
-        input_dir_str = inputarg
-    elif not(cmd_args.input is None):
-        input_dir_str: str = cmd_args.input
-    else:
-        input_dir_str = input('Where is your input folder, with the subfolder /maps? \n')
-    
     print('Input folder: ', input_dir_str)
-
-    #If there is no output parameter, ask
-    if outputarg != '':
-        output_dir_str = outputarg
-    elif not(cmd_args.output is None):
-        output_dir_str: str = cmd_args.output
-    else:
-        output_dir_str = input('Where is your output folder, preferably empty. \n')
+    print('Output folder: ', output_dir_str)
     
     # Checks
-    print('Output folder: ', output_dir_str)
     if(not(check_output_dir(output_dir_str))):
+        print('Make sure the output exists and is writable')
         sys.exit(2)
     if(not(check_input_dir(input_dir_str))):
+        print('Make sure the input exists and has a maps/ subfolder and .bsp files')
         sys.exit(1)
     
     is_parallel = not cmd_args.singlethread
